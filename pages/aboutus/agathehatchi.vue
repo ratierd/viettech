f4170036caa913c5d2dbe1e1abf917e879885afa
zenkage <ratierd@gmail.com>
2018-10-04 12:01:35 +0200

    Update agathe profile

--- pages/aboutus/agathehatchi.vue
+++ pages/aboutus/agathehatchi.vue
@@ -10,7 +10,7 @@
             <div class="uk-width-expand@m" data-editable data-name="person-bio">
                 <h3>Agathe HATCHI</h3>
                 <p style="display:flex;justify-content:flex-start;flex-wrap:wrap"><span><a href="https://www.linkedin.com/in/agathe-hatchi-1187b912b/" target="blank"><span uk-icon="icon: linkedin; ratio: 1"></span></a></span></p>
-				<p></p>
+				<p>Graduated with a bachelor's degree in physics chemistry, I completed my studies with a master's degree in project management, entrepreneurship and innovation at the Foreign Trade University of Hanoi in collaboration with the University of Nantes. This project, is a personal project undertaken with two friends to introduce people to Hanoi's innovative ecosystem. This is only the beginning of the project that I hope will be pursued by MPIE Master students in Vietnam to highlight Hanoi's innovative ecosystem.</p>
             </div>
         </div>
     </div>
